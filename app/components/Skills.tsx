3cf070e02bf2c12fa59f40a98734c666522400b1
Rafal Opiela <opiela@tlen.pl>
2023-05-06 10:35:18 +0200

    Updating skills

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -17,7 +17,7 @@ export default function Skills() {
           </div>
 
           <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">HTML5</span>
+            <span className="text-base font-medium">HTML5 / CSS3</span>
             <span className="text-sm font-medium">60%</span>
           </div>
           <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
@@ -41,7 +41,7 @@ export default function Skills() {
           </div>
 
           <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">CSS3</span>
+            <span className="text-base font-medium">PHP</span>
             <span className="text-sm font-medium">60%</span>
           </div>
           <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
@@ -57,15 +57,15 @@ export default function Skills() {
           </div>
 
           <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">Git</span>
-            <span className="text-sm font-medium">40%</span>
+            <span className="text-base font-medium">Python</span>
+            <span className="text-sm font-medium">50%</span>
           </div>
           <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
-            <div className="bg-red-600 h-3" style={{ width: '40%' }}></div>
+            <div className="bg-red-600 h-3" style={{ width: '50%' }}></div>
           </div>
 
           <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">Python</span>
+            <span className="text-base font-medium">Git</span>
             <span className="text-sm font-medium">40%</span>
           </div>
           <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
@@ -89,15 +89,7 @@ export default function Skills() {
           </div>
 
           <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">SQL</span>
-            <span className="text-sm font-medium">30%</span>
-          </div>
-          <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
-            <div className="bg-red-600 h-3" style={{ width: '30%' }}></div>
-          </div>
-
-          <div className="flex justify-between mb-1 mt-10">
-            <span className="text-base font-medium">Windows</span>
+            <span className="text-base font-medium">SQL / SqLite / MySQL</span>
             <span className="text-sm font-medium">30%</span>
           </div>
           <div className="w-full bg-gray-200 h-3 dark:bg-gray-700">
@@ -120,7 +112,7 @@ export default function Skills() {
             <div className="bg-red-600 h-3" style={{ width: '20%' }}></div>
           </div>
 
-          <p className="mt-10 text-justify">These are a selected technologies which I have used recently. I have also experience with Awk, Ruby, C/C++, PHP, Groovy, Java, C#, MongoDB. I also have theoretical knowledge of the basics of many other technologies such as Ansible, Jenkins, Terraform, Kubernetes, Helm. In the future I would like to become more familiar with Go, Scala and Rust etc.</p>
+          <p className="mt-10 text-justify">These are a selected technologies which I have used recently. I have also experience with Awk, Ruby, Groovy, Java, C/C++, C#, PostgreSQL, MongoDB etc. I also have theoretical knowledge of the basics of many other technologies such as Ansible, Jenkins, Terraform, Kubernetes, Helm. In the future I would like to become more familiar with Go, Scala and Rust etc.</p>
           <p className="mt-10 text-justify">I'm a person who is independent during solving IT problems. I'm ready to improve my qualifications and further learning.</p>
         </div>
         <div className="w-1/2 mt-10 sm:hidden flex mx-auto">
